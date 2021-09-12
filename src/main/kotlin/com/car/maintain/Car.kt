4488cc24c7ee7b99c8aced5d5a22e9079916a1f0
Meetsoni1206 <32540392+Meetsoni1206@users.noreply.github.com>
2021-09-12 13:01:47 +0530

    Class and method access

--- src/main/kotlin/com/car/maintain/Car.kt
+++ src/main/kotlin/com/car/maintain/Car.kt
@@ -1,11 +1,22 @@
 package com.car.maintain
 
-class Car(val type:String, val model:Int, val price:Double
-               ,val milesDrives:Int, val owner:String ) {
+class Car(
+    var type:String, val model:Int, val price:Double
+    , val milesDrives:Int, val owner:String ) {
     init {
         println("Object class is created")
     }
+    fun getCarPrice():Double{
+            return this.price - (this.milesDrives.toDouble() *20)
+    }
 }
 fun main(){
-    val car = Car("BMW",2012,20000.0,120,"Meet Soni")
+    var car = Car("BMW",2012,20000.0,120,"Meet Soni")
+    println(car.type)
+    println(car.price)
+    car.type = "Audi"
+    println(car.type)
+    var currentPrice = car.getCarPrice()
+    println("Current car Price : $currentPrice")
 }
+
